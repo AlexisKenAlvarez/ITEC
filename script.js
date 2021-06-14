d5084047f977a404996d5176d1aeb9a743f6dc5b
Amateurz7 <alexisken1432@gmail.com>
2021-06-14 21:00:21 +0800

    phone optimized

--- script.js
+++ script.js
@@ -14,9 +14,11 @@ window.addEventListener('scroll', () => {
   if (pageYOffset > 50) {
     arrowTop.style.opacity = "1";
     arrowTop.style.transform = "rotate(-90deg)";
+    arrowTop.style.cursor = "pointer";
   } else {
     arrowTop.style.opacity = "0";
     arrowTop.style.transform = "rotate(180deg)";
+    arrowTop.style.cursor = "";
   }
 });
 
